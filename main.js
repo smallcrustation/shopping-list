883f0f2460dc750eae2803e73fda15f8bc822585
krill <lpmaze@gmail.com>
2019-06-19 15:02:03 -0400

    added hide-check button, fixed submit item allowing blank and whitespace

--- main.js
+++ main.js
@@ -11,26 +11,27 @@ function handleAddItemSubmit() {
   $('#js-shopping-list-form').submit((e) => {
     e.preventDefault(); // prevent default html submit action
     // get the item name fro the form
-    const item = $('.js-shopping-list-entry').val();
-    $('.js-shopping-list-entry').val(''); //clear text in input box
-    // addItemToShoppingList and refresh the display
-    addItemToShoppingList(item);
-    handleDisplayListItems();
+    const itemName = $('.js-shopping-list-entry').val().trim();
+    if (itemName.length > 0 ){
+      $('.js-shopping-list-entry').val(''); //clear text in input box
+      // addItemToShoppingList and refresh the display
+      addItemToShoppingList(itemName);
+      handleDisplayListItems();
+    }
   });
 }
 
-
 function addItemToShoppingList(itemName) {
-  store.items.push({ 
-    itemName: itemName, 
-    checked: false, 
+  store.items.push({
+    itemName: itemName,
+    checked: false,
     id: 0,
   });
 }
 
 // handle remove item
-function handleItemDelete(){
-  $('.js-shopping-list').on('click', '.js-shopping-item-delete', function() { // why dsnt .click work?
+function handleItemDelete() {
+  $('.js-shopping-list').on('click', '.js-shopping-item-delete', function () { // why dsnt .click work?
     const index = getItemById($(this).attr('value'));
     store.items.splice(index, 1);
     // refresh list items
@@ -38,23 +39,26 @@ function handleItemDelete(){
   });
 }
 
-
 // handle check/un-check item
-function handleItemToggle(){
-  $('.js-shopping-list').on('click', '.js-shopping-item-toggle', function(){
+function handleItemToggle() {
+  $('.js-shopping-list').on('click', '.js-shopping-item-toggle', function () {
     const index = getItemById($(this).attr('value'));
     store.items[index].checked = !store.items[index].checked;
     // refresh list items
     handleDisplayListItems();
-  }); 
+  });
 }
 
 // find an item based on item.id, return item index
-function getItemById(val){
+function getItemById(val) {
   let id = parseInt(val); // why can't i do .on('click', console.log())
   let index = store.items.findIndex(item => item.id === id);
   return index;
 }
+
+// handle display only unchecked items
+
+
 
 // handle search
 
@@ -73,24 +77,24 @@ function handleDisplayListItems() {
       listItem.id = i;
       $('.js-shopping-list').append(generateListElement(listItem));
     }
-  } else{
+  } else {
     $('.js-shopping-list').empty();
   }
 }
 
 function generateListElement(listItem) {
   return (`<li>
-  <span class="shopping-item ${listItem.checked ? 'shopping-item__checked' : ''}">
-  ${listItem.itemName}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle js-shopping-item-toggle" value="${listItem.id}">
-      <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete js-shopping-item-delete" value="${listItem.id}">
-      <span class="button-label">delete</span>
-    </button>
-  </div>
-</li>`);
+    <span class="shopping-item ${listItem.checked ? 'shopping-item__checked' : ''}">
+    ${listItem.itemName}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-shopping-item-toggle" value="${listItem.id}">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-shopping-item-delete" value="${listItem.id}">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
 }
 
 // handle shopping list
